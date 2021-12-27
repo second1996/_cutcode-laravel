ea8b541d89190bf727c18b842f01fa36c7d3d93a
second1996 <vital10012@gmail.com>
2021-12-27 16:18:48 +0200

    Update

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-	
+	const burgerMenu = document.querySelector('#burgerMenu');
+	const mobileMenu = document.querySelector('#mobileMenu');
+	const closeMobileMenu = document.querySelector('#closeMobileMenu');
+
+	function toggleMobileMenu() {
+		mobileMenu.classList.toggle('hidden');
+	}
+
+	burgerMenu.addEventListener('click', function(event) {
+		toggleMobileMenu();
+	});
+
+	closeMobileMenu.addEventListener('click', function(event) {
+		toggleMobileMenu();
+	});
 })
