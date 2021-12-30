ecbf5803edec3303008c770a5406d432cb1df33b
second1996 <vital10012@gmail.com>
2021-12-30 17:29:19 +0200

    Update

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+	/**
+	 * Mobile menu
+	 */
 	const burgerMenu = document.querySelector('#burgerMenu');
 	const mobileMenu = document.querySelector('#mobileMenu');
 	const closeMobileMenu = document.querySelector('#closeMobileMenu');
@@ -14,4 +18,23 @@ document.addEventListener('DOMContentLoaded', function() {
 	closeMobileMenu.addEventListener('click', function(event) {
 		toggleMobileMenu();
 	});
+
+	/**
+	 * Vacancies slider
+	 */
+	const vacanciesSwiper = new Swiper('.s-vacancies-slider', {
+		slidesPerView: 2,
+		slidesPerGroup: 2,
+		spaceBetween: 40,
+		pagination: {
+			el: '.s-vacancies-slider .swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.s-vacancies-slider-arrows .swiper-button-next',
+			prevEl: '.s-vacancies-slider-arrows .swiper-button-prev',
+		},
+	});
+	
+
 })
