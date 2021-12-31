0279f2a8d6b151ea2f9fcb23267155cf8a8dba4a
second1996 <vital10012@gmail.com>
2021-12-31 12:25:27 +0200

    Update

--- app/js/app.js
+++ app/js/app.js
@@ -23,9 +23,10 @@ document.addEventListener('DOMContentLoaded', function() {
 	 * Vacancies slider
 	 */
 	const vacanciesSwiper = new Swiper('.s-vacancies-slider', {
-		slidesPerView: 2,
-		slidesPerGroup: 2,
+		slidesPerView: 1,
+		slidesPerGroup: 1,
 		spaceBetween: 40,
+		autoHeight: true,
 		pagination: {
 			el: '.s-vacancies-slider .swiper-pagination',
 			clickable: true,
@@ -33,6 +34,39 @@ document.addEventListener('DOMContentLoaded', function() {
 		navigation: {
 			nextEl: '.s-vacancies-slider-arrows .swiper-button-next',
 			prevEl: '.s-vacancies-slider-arrows .swiper-button-prev',
+		},
+		breakpoints: {
+			1140: {
+				slidesPerView: 2,
+				slidesPerGroup: 2,
+				autoHeight: false,
+			},
+		},
+	});
+	
+
+	/**
+	 * Testimonials slider
+	 */
+	const testimonialsSwiper = new Swiper('.s-testimonials-slider', {
+		slidesPerView: 1,
+		slidesPerGroup: 1,
+		spaceBetween: 40,
+		autoHeight: true,
+		pagination: {
+			el: '.s-testimonials-slider .swiper-pagination',
+			clickable: true,
+		},
+		navigation: {
+			nextEl: '.s-testimonials-slider-arrows .swiper-button-next',
+			prevEl: '.s-testimonials-slider-arrows .swiper-button-prev',
+		},
+		breakpoints: {
+			960: {
+				slidesPerView: 2,
+				slidesPerGroup: 2,
+				autoHeight: false,
+			},
 		},
 	});
 	
--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
 <h1>Landing page для CutCode</h1>
-<p>Solid Code  в Laravel — Видеокурс по изучению принципов программирования</p>
+<p>Solid Code в Laravel — Видеокурс по изучению принципов программирования</p>
+
+<h2>Generate fonts on the first run:</h2>
+<code>gulp fonts</code>
 
 <h2>Watch in-real time Tailwind:</h2>
 <code>npx tailwindcss -i ./app/sass/tailwind.css -o ./dist/css/tailwind.css --watch</code>
